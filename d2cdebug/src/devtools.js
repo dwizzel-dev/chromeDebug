73add2375cea040913c31d95dd77d4bd27a5a13c
dwizzel-dev <dwizzel.dev@gmail.com>
2018-07-28 19:43:51 -0400

    lets eat

--- d2cdebug/src/devtools.js
+++ d2cdebug/src/devtools.js
@@ -1,47 +1,112 @@
-// ce script est caller qusn le panel est active
+// ce script est caller quand le panel est active
 
 //import Const from './constant';
+
+console.log('[devtool.js] loaded');
 
 const Const = {
   INIT: 0x201,
   INJECT: 0x202,
   CLEAR: 0x203,
   LOADING: 0x204,
+  EMPTY: 0x205,
   ONDATA: 0x302,
   TOOLNAME: 'd2cmedia-devtool-inspector',
   CONTENTNAME: 'd2cmedia-devtool-content'
 };
 
-console.log('[devtool.js] loaded');
+const Consolas = {
+  log: function(data){
+    if(this.options.enabled){
+      console.log(data);
+    }
+  },
+  warn: function(data){
+    if(this.options.enabled){
+      console.warn(data);
+    }
+  },
+  error: function(data){
+    if(this.options.enabled){
+      console.error(data);
+    }
+  },
+  options: {
+      enabled: true,
+  },
+};
+
+const PanelWriter = {
+  wrtMsg: data => `<span class="msg">${data}</span>`,
+  wrtOutput: data => {
+    const output = document.querySelector('.container');
+    if(typeof output !== 'undefined'){
+      output.innerHTML = `<div class="content">${data}</div>`;
+    }
+  },
+  wrtRow: (self, data) => {
+    return `<div class="row">
+      <h2><span class="classname">${data.from.class} :: </span>${data.from.method}</h2>  
+      <h3>${data.from.file} <span class="line">${data.from.line}</span></h3>  
+      <div class="cell code"><xmp>${self.wrtObj(data.obj)}</xmp></div>
+      <div class="cell filter">filter: ${data.filter}</div>
+      </div>`;
+  },
+  wrtObj: data => {
+    return `${data}`;
+  },
+  wrt: function(data){
+    Consolas.log('[devtool.js] data received');
+    Consolas.log(data);
+    let content = '';
+    switch(data){
+      case Const.CLEAR:
+        content = this.wrtMsg('Not a D2CMedia debuggable');
+        break; 
+      case Const.LOADING:
+        content = this.wrtMsg('Loading...');
+        break;
+      case Const.EMPTY:
+        content = this.wrtMsg('Empty Data');
+        break;   
+      default:
+        for(let k in data){
+          content += this.wrtRow(this, data[k]);
+        }
+        break;  
+    }
+    this.wrtOutput(content);
+  },
+};
 
 //creer la connection a devtools-background.js
-console.log('[devtool.js] connecting to [devtools-background.js]');
+Consolas.log('[devtool.js] connecting to [devtools-background.js]');
 var bgPageConnection = chrome.runtime.connect({
   name: 'devtool-d2cmedia'
 });
 
 //ecoute pour les message de la devtools-background.js
 bgPageConnection.onMessage.addListener(message => {
-  console.log('[devtool.js] message from [devtools-background.js]');
-  console.log(message);
+  Consolas.log('[devtool.js] message from [devtools-background.js]');
+  Consolas.log(message);
   //message from the devtools-background.js to inject javascript code to the content
   if(typeof message.name !== 'undefined' && typeof message.command !== 'undefined'){
     if(message.name == Const.TOOLNAME){
       switch(message.command){
         case Const.INJECT:
-          console.log('[devtool.js] injecting "backend.js" to inspected content page');
+          Consolas.log('[devtool.js] injecting "backend.js" to inspected content page');
           //injecte le script backend.js dans la content-page (page web que lon inspect) et attend le result
           injectScript(chrome.runtime.getURL('src/backend.js'), res => {
-            console.log('[devtool.js] injection callback');
+            Consolas.log('[devtool.js] injection callback');
           });
           break;
         case Const.CLEAR:
-          console.log('[devtool.js] clearing panel page content');
-          writeDataToPanel(false);
+          Consolas.log('[devtool.js] clearing panel page content');
+          PanelWriter.wrt(Const.CLEAR);
           break;  
         case Const.LOADING:
-          console.log('[devtool.js] clearing panel page content');
-          writeDataToPanel(Const.LOADING);
+          Consolas.log('[devtool.js] clearing panel page content');
+          PanelWriter.wrt(Const.LOADING);
           break;    
         default:
           break;  
@@ -49,8 +114,12 @@ bgPageConnection.onMessage.addListener(message => {
     }else if(message.name == Const.CONTENTNAME){
       switch(message.command){
         case Const.ONDATA:
-          console.log('[devtool.js] writing to panel');
-          writeDataToPanel(message.data);
+          Consolas.log('[devtool.js] writing to panel');
+          if(message.data.length === 0){
+            PanelWriter.wrt(Const.EMPTY);
+          }else{
+            PanelWriter.wrt(message.data);  
+          }
           break;
         default:
           break;  
@@ -61,12 +130,12 @@ bgPageConnection.onMessage.addListener(message => {
 
 //ecoute les deconnxion de la devtools-background.js
 bgPageConnection.onDisconnect.addListener(message => {
-  console.log('[devtool.js] disconnection of [devtools-background.js]');
-  console.log(message);
+  Consolas.log('[devtool.js] disconnection of [devtools-background.js]');
+  Consolas.log(message);
 });
 
 //envoie un message a la devtools-background.js
-console.log('[devtool.js] sending init signal to [devtools-background.js]');
+Consolas.log('[devtool.js] sending init signal to [devtools-background.js]');
 bgPageConnection.postMessage({
   name: Const.TOOLNAME,
   command: Const.INIT,
@@ -87,37 +156,8 @@ function injectScript(scriptName, cb){
     `
   chrome.devtools.inspectedWindow.eval(src, (res, err) => {
     if (err) {
-      console.log(err)
+      Consolas.log(err)
     }
     cb(res);
   })
-}
-
-//write tot the panel
-function writeDataToPanel(data){
-  console.log('[devtool.js] data received');
-  console.log(data);
-  // HTML element to output our data.
-  const output = document.querySelector('.container');
-  let html = '<div class="content">';
-  if(data === false){
-    html += '<span class="msg">Not a D2CMedia Debugable page</span>';
-  }else if(data === Const.LOADING){
-      html += '<span class="msg">Loading...</span>';
-  }else if(data.lenght <= 0){
-    html += '<span class="msg">No content to show</span>'
-  }else{
-    for(let o in data){
-      html += '<div class="row">';
-      html += `<div class="cell method"><span class="classname">${data[o]['from']['class']}::</span>${data[o]['from']['method']}</div>`;  
-      html += `<div class="cell file">${data[o]['from']['file']} <span class="line">${data[o]['from']['line']}</span></div>`;  
-      //html += `<div class="cell code"><xmp>${JSON.stringify(data[o]['obj'])}</xmp></div>`;  
-      html += `<div class="cell code"><xmp>${data[o]['obj']}</xmp></div>`;  
-      html += `<div class="cell filter">filter: ${data[o]['filter']}</div>`;  
-      html += '</div>';
-    }
-  }
-  html += '</div>';
-  output.innerHTML = html;
-
 }
